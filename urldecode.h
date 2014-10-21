075755bcd98e1e11eb95ed347869d2d75013fd19
Chris Snow <chsnow@gmail.com>
2014-10-21 00:22:06 -0700

    TODO re URL decoding

--- urldecode.h
+++ urldecode.h
@@ -1,6 +1,9 @@
 #ifndef _URLDECODE_H_
 #define _URLDECODE_H_
 
+// TODO(chsnow): implement URL decoding routine, so we can support upload of new 
+// lighting programs via HTTP GET with params.
+
 // http://forum.arduino.cc/index.php/topic,99538.0.html
 
 #endif  // _URLDECODE_H_ 
